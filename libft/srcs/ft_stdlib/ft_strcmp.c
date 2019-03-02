184be0d328ec6465f7819b572ea5ef1cbda4b528
Zhang WENG <zweng@e3r4p7.42.fr>
2019-03-02 15:26:26 +0100

    norm

--- libft/srcs/ft_stdlib/ft_strcmp.c
+++ libft/srcs/ft_stdlib/ft_strcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zweng <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: zweng <zweng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/09 16:42:33 by zweng             #+#    #+#             */
-/*   Updated: 2017/11/09 16:59:43 by zweng            ###   ########.fr       */
+/*   Created: 2019/03/02 15:26:06 by zweng             #+#    #+#             */
+/*   Updated: 2019/03/02 15:26:07 by zweng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-    if (!s1 && !s2)
-        return (0);
-    if (!s1)
-        return (-1);
-    if (!s2)
-        return (1);
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (-1);
+	if (!s2)
+		return (1);
 	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
